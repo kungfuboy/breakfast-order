2c9711a32de54400f627e728053c37d9af05fa26
dark <dark@dark.lan>
2019-08-23 16:20:00 +0800

    uu

--- server/router.js
+++ server/router.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const Router = require('koa-router')
 const router = new Router()
 const Redis = require('koa-redis')
--- server/server.js
+++ server/server.js
@@ -4,8 +4,24 @@ const Koa = require('koa')
 const cors = require('koa2-cors')
 const bodyParser = require('koa-bodyparser')
 const router = require('./router')
+const Redis = require('koa-redis')
+
+const Store = new Redis().client
 
 const app = new Koa()
+const time = 604800000
+
+const timer = () => {
+  setInterval(async () => {
+    const arr = await Store.hkeys('order')
+    const res = await Promise.all(arr.map(async item => ({
+        name: unescape(item),
+        order: JSON.parse(await Store.hget('order', item))
+    })))
+    console.log(res)
+    Store.del('order')
+  }, time)
+}
 
 app
   .use(cors()) // 允许跨域
@@ -15,5 +31,6 @@ app
 
 // 监听
 app.listen(3030, () => {
+  timer()
   console.log('Listening 3030...')
 })
